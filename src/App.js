6d1a02c2e11b869f73aa34692be265ea9aa2257b
Amit Chaudhary <itheamc@gmail.com>
2022-04-30 21:47:31 +0545

    Initial Commit - Project Setup

--- src/App.js
+++ src/App.js
@@ -1,57 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom'
 import './App.css';
+import HomePage from './pages/home/HomePage';
+import OrdersPage from './pages/orders/OrdersPage';
+import ProductsPage from './pages/products/ProductsPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Router>
+      <div className="main-container">
+        <div className="side-menu-container">
+          <div className="side-menu">
+            <ul>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/orders">Orders</NavLink>
+              </li>
+              <li>
+                <NavLink to="/products">Products</NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="body-content-container">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="products" element={<ProductsPage />} />
+            <Route path="orders" element={<OrdersPage />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
